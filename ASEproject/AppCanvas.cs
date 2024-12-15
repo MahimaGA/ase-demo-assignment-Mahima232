bb4d56ea8318af9965b584eba5aacf98e928d2dd
Mahima GA <yourpersonalemail@example.com>
2024-12-15 11:01:12 +0545

    Add new class and implement ICanvas

---
+++ ASEproject/AppCanvas.cs
@@ -0,0 +1,71 @@
+﻿using BOOSE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEproject
+{
+    internal class AppCanvas : ICanvas
+    {
+        public int Xpos { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Ypos { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object PenColour { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Circle(int radius, bool filled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawTo(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object getBitmap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rect(int width, int height, bool filled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetColour(int red, int green, int blue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Tri(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteText(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
