46681de22a48a4925282213aa4742df2b579aa67
Mahima GA <yourpersonalemail@example.com>
2024-12-22 11:41:40 +0545

    Implement basic drawing functionality

--- ASEproject/Form1.cs
+++ ASEproject/Form1.cs
@@ -5,10 +5,34 @@ namespace ASEproject
 {
     public partial class Form1 : Form
     {
+        Parser parser;
+        CommandFactory commandFactory;
+        ICanvas canvas;
+        StoredProgram storedProgram;
+
         public Form1()
         {
             InitializeComponent();
             Debug.WriteLine(AboutBOOSE.about());
+            canvas = new AppCanvas();
+            commandFactory = new CommandFactory();
+            storedProgram = new StoredProgram(canvas);
+            parser = new Parser(commandFactory, storedProgram);
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            String text = ProgramWindow.Text;
+            parser.ParseProgram(text);
+            storedProgram.Run();
+            Refresh();
+        }
+
+        private void OutputWindow_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g =e.Graphics;
+            Bitmap b = (Bitmap) canvas.getBitmap();
+            g.DrawImageUnscaled(b, 0, 0);
         }
     }
 }
