271836dc9a3301ffe71f5815869da8b5032272a7
Mahima GA <mahimaagarwal285@gmail.com>
2024-12-23 09:15:08 +0545

    Add reset and clear

---
+++ ASEproject/Clear.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOOSE;
+
+public class Clear : CommandOneParameter
+    {
+
+        private string[] parameters;
+
+        public Clear()
+        {
+        }
+        public Clear(Canvas canvas) : base(canvas)
+        {
+
+        }
+
+
+        public override void Execute()
+        {
+
+            canvas.Clear();
+            string message = "Canvas Clear successfully";
+            MessageBox.Show(message, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        public void CheckParameters(string[] Parameters)
+        {
+
+            if (Parameters.Length > 0)
+                throw new ArgumentException("Clear command does not require any parameters.");
+        }
+    }
---
+++ ASEproject/Reset.cs
@@ -0,0 +1,37 @@
+﻿using BOOSE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Reset : CommandOneParameter
+{
+
+    private string[] parameters;
+
+    public Reset()
+    {
+    }
+    public Reset(Canvas canvas) : base(canvas)
+    {
+
+    }
+
+
+    public override void Execute()
+    {
+
+        canvas.Reset();
+        string message = "Canvas reset successfully";
+        MessageBox.Show(message, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+
+    public void CheckParameters(string[] Parameters)
+    {
+
+        if (Parameters.Length > 0)
+            throw new ArgumentException("Reset command does not require any parameters.");
+    }
+}
