d88792a0390b2e127b96fdc271d2c15de63a0e80
Mahima GA <mahimaagarwal285@gmail.com>
2024-12-24 07:46:47 +0545

    Add functions reset and clear

--- ASEproject/Reset.cs
+++ ASEproject/Reset.cs
@@ -5,20 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// represents a command that resets
+/// </summary>
 public class Reset : CommandOneParameter
 {
 
     private string[] parameters;
 
+    /// <summary>
+    /// initializes a new instance of the <see cref="Reset"/> class
+    /// </summary>
     public Reset()
     {
     }
+
+    /// <summary>
+    /// initializes a new instance of the <see cref="Reset"/> class with canvas
+    /// </summary>
+    /// <param name="canvas">the canvas to reset</param>
     public Reset(Canvas canvas) : base(canvas)
     {
 
     }
 
-
+    /// <summary>
+    /// executes reset command
+    /// </summary>
     public override void Execute()
     {
 
@@ -27,7 +40,11 @@ public class Reset : CommandOneParameter
         MessageBox.Show(message, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
     }
 
-
+    /// <summary>
+    /// validates that no parameter is provided
+    /// </summary>
+    /// <param name="Parameters">parameters to validate</param>
+    /// <exception cref="ArgumentException">thrown when parameter given for reset</exception>
     public void CheckParameters(string[] Parameters)
     {
 
