7f85cb9d5378e81fd784bb6d7475658ec2490b97
Mahima GA <yourpersonalemail@example.com>
2025-01-14 23:07:33 +0545

    Complete replaced if, while, for

---
+++ ASEproject/AppWhile.cs
@@ -0,0 +1,23 @@
+﻿using BOOSE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEproject
+{
+    public class AppWhile : While
+    {
+        public AppWhile()
+        {
+            ReduceRestrictions();
+
+        }
+
+        public override void Restrictions()
+        {
+
+        }
+    }
+}
